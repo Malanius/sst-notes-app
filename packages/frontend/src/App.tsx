66370aac6989640dc208c512dab76864c2044532
Malanius Privierre <malaniusprivierre@gmail.com>
2023-09-30 17:03:58 +0200

    feat(fe): add sing-up and login links to navbar

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,4 +1,7 @@
+import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
+
 import "./App.css";
 import Routes from "./Routes.tsx";
 
@@ -6,8 +9,20 @@ function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
-        <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
